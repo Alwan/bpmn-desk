e300171043275157e6b1dec96a852ef0fb794e36
alwan <alwan.sa@gmail.com>
2026-09-14 11:36:16 +0300

    Add BPMN-themed app icon (SVG source + PNG/ICO sets)

--- src/main.js
+++ src/main.js
@@ -10,6 +10,7 @@ function createWindow() {
     width: 1400,
     height: 900,
     title: "bpmn-desk",
+    icon: path.join(__dirname, "..", "build", "icon.png"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       contextIsolation: true,
